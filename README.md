f404337854b5c94fcd3e95f7beb8637ce736e558
cadence <csteward24@gmail.com>
2018-02-02 14:35:36 -0500

    foo

--- README.md
+++
@@ -1,1 +0,0 @@
-#This repository is meant to store sumo routines.
---
+++ Timer.cpp
@@ -0,0 +1,35 @@
+#include <Zumo32U4.h>
+//this code is so bad---NEVER DO THIS IRL PLS!! -CS
+class Timer{
+  int startTime = millis();
+  int stage = 0;
+  public:
+    boolean isSet = false;
+    void startTimerC(){
+      if(!isSet){
+        startTimer();
+      }
+    }
+    void startTimer(){
+      startTime = millis();
+      isSet = true;
+    }
+    int timeElapsed(){
+      if(!isSet)
+        return 0;
+      return millis() - startTime;
+    }
+    void reset(){
+      isSet = false;
+    }
+    void setStage(int stage){
+      this->stage = stage;
+    }
+    int getStage(){
+      return stage;
+    }
+    void nextStage(){
+      stage++;
+    }
+};
+
