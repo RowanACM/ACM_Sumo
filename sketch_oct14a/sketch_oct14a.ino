2d84e32daf84fc239c10c04fe2ac9a7d0638a1e6
maxlliiw <maxlliiw@users.noreply.github.com>
2018-10-15 17:04:55 -0400

    Bach's Fugue in D minor

---
+++ sketch_oct14a/sketch_oct14a.ino
@@ -0,0 +1,10 @@
+#include <PololuBuzzer.h>
+PololuBuzzer buzzer;
+
+void setup() {
+  buzzer.play("!L16 V16 T112 O2 aa O3 arerre-rdrcc O2 a O3 cd O2 aa O3 arerre-rdrcc O2 a O3 cd O2 aa O3 arerre-rdrcc O2 a O3 cd O2 aa O3 arerre-rdrcc O2 a O3 cd O2 aa O3 arerre-rdrcc O2 a O3 cd O2 aa O3 arerre-rdrcc O2 a O3 cd O2 aa O3 arerre-rdrcc O2 a O3 cd O2 aa O3 aaarerre-ddrcrcdggaerre-ddrgee-dg-g-arerre-rdrcc O2 a O3 cde-dcd8c O2 a O3 cc8 O2 L8 baa- O4 L16 ccccrcrc8 O3 araa4 O4 ccccrdr L8 e- L16 dc O3 a O4 cdrrccccrdre-eergree8 O3 a8araeagg2 O4 ccccrcrc8 O3 brb4rr O4 ccccrdr L8 e- L16 dc O3 a O4 cdrrc8 O3 b8a8g8 O5 c8 O4 b8a8g8 O3 f8ga O4 c O3 bb2 O2 a2 O3 c O2 a O3 cde-dc O2 a O3 L32 e-dc O2 a O3 L16 c8d2 L8 e- L16 eg8ee-dc O2 ab O3 c4d4e4g4a2c8d8e8 O4 c8 O3 b4a4b4 O4 c4d4 O3 b4 O4 e L32 ee-dd-c O3 bb-aa-gg-f L16 f1 O2 aa O3 arerre-rdrcc O2 a O3 cd O2 aa O3 arerre-rdrcc O2 a O3 cd O2 aa O3 arerre-rdrcc O2 a O3 cd O2 aa O3 arerre-rdrcc O2 a O3 cd O2 aa O3 arerre-rdrcc O2 a O3 cd O2 aa O3 arerre-rdrcc O2 a O3 cd O2 aa O3 arerre-rdrcc O2 a O3 cd O2 f1 O3 c4 O2 a2b2 O3 c1r1 O2 f1 O3 c4 O2 a2b2 O3 c1r1 O2 ff O3 arerre-rdrcc O2 a O3 cd O2 gg O3 arerre-rdrcc O2 a O3 cd O2 gg O3 arerre-rdrcc O2 a O3 cd O2 gg O3 arerre-rdrcc O2 a O3 cd O2 gg O3 arerre-rdrcc O2 a O3 cd O3 a O2 a O3 arerre-rdrcc O2 a O3 cdggarerre-rdrg8ee-dg-g-arerre-rdrcc O2 a O3 cde-dcddc O2 a O3 cc2rrc O2 aa8g8ga O3 c O2 ga4g8g8gagr O3 L8 e- L16 O2 g O3 dd8cr O2 aara8 O3 edeg4e4d8 L4 e- L16 d4c8e8 O2 b8 O3 c8cda O2 g O3 grg8aggga O4 crcdcrrcr O3 agare-dc O2 gg O3 gg-fe-raraged L4 e-d L16 c8c4a4g8g2ga-arg8a8g8ra8aagcrcre8c8gg-g O4 d2 O3 e8 L32 fe L16 d4c8 L4 e-d L16 c8g4g4a8b8a8gager1 O1 a8a8aa8ab8b8bb8b O2 c8c8cc8cd8d8dd8d O1 a8a8aa8ab8b8bb8b O2 c8c8cc8cd8d8dd8d O1 a8a8aa8ab8b8bb8b O2 c8c8cc8cd8d8dd8d O1 a8a8aa8ab8b8bb8b O2 c8c8cc8cd8d8dd8d");
+}
+
+void loop() {
+  
+}
