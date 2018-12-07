8dacce458ab37af76ca11ccd81f965c8c0b38b2f
cadence <csteward24@gmail.com>
2018-12-07 15:18:35 -0500

    add robot.h

--- sumo4/Robot.cpp
+++ sumo4/Robot.cpp
@@ -3,28 +3,8 @@
 #include "TurnSensor.h"
 #include "Timer.h"
 class Robot{
-    int lineReadings[5];
-    int leftReading;
-    int rightReading;
-    const int MAX_SPEED = 400;
-    boolean firstAtLineFlag;
-    Timer atLineTimer;
-  public:
-    enum class State {init, wait, search, attack, evade, atLine};
-    State state = State::init;
-    Zumo32U4ProximitySensors proxSensors;
-    Zumo32U4Motors motors;
-    Zumo32U4ButtonA buttonA;
-    Zumo32U4ButtonB buttonB;
-    Zumo32U4LineSensors lineSensors;
-    Zumo32U4LCD lcd;
-    L3G gyro;
-
-    Timer waitTimer;
-    Timer turnTimer;
-    TurnSensor turnSen;
-
     
+  public:
     void update(){
       lineSensors.readCalibrated(lineReadings);
       proxSensors.read();
---
+++ sumo4/Robot.h
@@ -0,0 +1,36 @@
+#pragma once
+class Robot{
+    int lineReadings[5];
+    int leftReading;
+    int rightReading;
+    const int MAX_SPEED = 400;
+    boolean firstAtLineFlag;
+    Timer atLineTimer;
+  public:
+    enum class State {init, wait, search, attack, evade, atLine};
+    State state = State::init;
+    Zumo32U4ProximitySensors proxSensors;
+    Zumo32U4Motors motors;
+    Zumo32U4ButtonA buttonA;
+    Zumo32U4ButtonB buttonB;
+    Zumo32U4LineSensors lineSensors;
+    Zumo32U4LCD lcd;
+    L3G gyro;
+
+    Timer waitTimer;
+    Timer turnTimer;
+    TurnSensor turnSen;
+
+    
+    void update();
+    void init();
+    void calibrateLineSensors();
+    void wait();
+    void checkLine();
+    void atLine();
+    void turnDeg(uint32_t turn);
+    void gambit();
+    void search();
+    void attack();
+    void displayProx();
+}
